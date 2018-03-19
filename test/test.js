432f0d60594ddce0e95a5f5e6dcc7836afceed91
Marcos <m_s_luciano92@hotmail.com>
2018-03-19 11:42:24 -0300

    Changed chai and mocha by jasmine

--- test/test.js
+++ test/test.js
@@ -1,7 +1,22 @@
 describe('My React Test', () => {
-    it('should find moana', (done) => {
-        browser.url('./');
-        browser.setValue('#q', 'Moana');
-        
-    });
-});
+  beforeEach(() => {
+    browser.url('./');
+    browser.waitForText('[data-reactroot]');
+  })
+
+  it('should find moana', () => {
+    browser.setValue('#q', 'moana');
+
+    browser.waitForText('.tt-dropdown-menu');
+
+    browser.keys('Down arrow');
+    browser.keys('Enter');
+
+    browser.pause(500);
+
+    let pageHeading = browser.getText('h1');
+    
+    expect(pageHeading).toBe('MOANA');
+    expect(browser.getText('.tagline')).toBe('The ocean is calling.');
+  })
+})
